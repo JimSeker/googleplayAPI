a274f4e96c45a70d6ec53f81c665b7d9c166646c
JimSeker <seker@uwyo.edu>
2017-04-14 15:20:24 -0600

    first stab at firebase messaging.

---
+++ FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.cs4730.firebasemessagedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// using https://www.simplifiedcoding.net/firebase-cloud-messaging-tutorial-android/
+//   and https://www.simplifiedcoding.net/firebase-cloud-messaging-android/
+//   and https://www.androidtutorialpoint.com/firebase/firebase-android-tutorial-getting-started/
+//   and https://www.androidtutorialpoint.com/firebase/firebase-cloud-messaging-tutorial/
+//   and https://firebase.google.com/docs/cloud-messaging/
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/MyFirebaseInstanceIDService.java
@@ -0,0 +1,32 @@
+package edu.cs4730.firebasemessagedemo;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Seker on 4/14/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
