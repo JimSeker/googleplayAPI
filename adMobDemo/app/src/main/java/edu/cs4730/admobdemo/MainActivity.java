c38d704b18a890130f917d1df4f1b8472e196b09
JimSeker <seker@uwyo.edu>
2017-11-06 17:47:50 -0700

    updated to googleplay 11.6.0

--- adMobDemo/app/src/main/java/edu/cs4730/admobdemo/MainActivity.java
+++ adMobDemo/app/src/main/java/edu/cs4730/admobdemo/MainActivity.java
@@ -1,13 +1,9 @@
 package edu.cs4730.admobdemo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
