3d69dbd4c48078af7021e94a790a0d8ce553cbc7
Jim Ward <seker@uwyo.edu>
2017-12-15 10:04:14 -0700

    added distance method and more comments.

--- NearbyMessagesDemo/app/src/main/java/edu/cs4730/nearbymessagesdemo/SubscribeFragment.java
+++ NearbyMessagesDemo/app/src/main/java/edu/cs4730/nearbymessagesdemo/SubscribeFragment.java
@@ -12,14 +12,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.BleSignal;
+import com.google.android.gms.nearby.messages.Distance;
 import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageFilter;
 import com.google.android.gms.nearby.messages.MessageListener;
 import com.google.android.gms.nearby.messages.Strategy;
 import com.google.android.gms.nearby.messages.SubscribeOptions;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * This class is the subscribe side of the code.  It will read the messages found.
+ * It has foreground for when the activity is activity and a background that uses the receiver.
+ *
+ * The foreground has has code to figure the distance of the beacon if possible.
+ *
  */
 public class SubscribeFragment extends Fragment {
 
@@ -32,6 +39,9 @@ public class SubscribeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    /**
+     * This is the listener for foreground messages.
+     */
     MessageListener mMessageListener = new MessageListener() {
         @Override
         public void onFound(Message message) {
@@ -42,13 +52,48 @@ public class SubscribeFragment extends Fragment {
         public void onLost(Message message) {
             logthis("Lost sight of message: " + new String(message.getContent()));
         }
+
+
+        /**
+         *
+         * Note the following two methods are not required and used for distance information.
+         *   one problem, we are not publishing a beacon message, so these are not called.
+         *   https://developers.google.com/beacons/proximity/attachments
+         *   https://developers.google.com/beacons/proximity/guides
+         *
+         * Called when the Bluetooth Low Energy (BLE) signal associated with a message changes.
+         *
+         * This is currently only called for BLE beacon messages.
+         *
+         * For example, this is called when we see the first BLE advertisement
+         * frame associated with a message; or when we see subsequent frames with
+         * significantly different received signal strength indicator (RSSI)
+         * readings.
+         *
+         * For more information, see the MessageListener Javadocs.
+         */
+        @Override
+        public void onBleSignalChanged(final Message message, final BleSignal bleSignal) {
+            logthis( "Message: " + message + " has new BLE signal information: " + bleSignal);
+        }
+
+        /**
+         * Called when Nearby's estimate of the distance to a message changes.
+         * This is currently only called for BLE beacon messages.
+         * For more information, see the MessageListener Javadocs.
+         */
+        @Override
+        public void onDistanceChanged(final Message message, final Distance distance) {
+            logthis( "Distance changed, message: " + message + ", new distance: " + distance);
+        }
+
     };
 
     // Subscribe to receive messages.
     private void subscribe() {
         //foreground subscription.
         logthis("Subscribing.");
-        SubscribeOptions options = new SubscribeOptions.Builder()
+         SubscribeOptions options = new SubscribeOptions.Builder()
             .setStrategy(Strategy.BLE_ONLY)
             .build();
         Nearby.getMessagesClient(getActivity()).subscribe(mMessageListener, options);
@@ -115,6 +160,7 @@ public class SubscribeFragment extends Fragment {
 
     }
 
+    //helper function to log and added to textview.
     public void logthis(String msg) {
         logger.append(msg + "\n");
         Log.d(TAG, msg);
