0a4e407987ee7963f60b501ae60481cefd789d5a
Jim Ward <seker@uwyo.edu>
2019-02-19 12:53:24 -0700

    updated to androidx

--- FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/MainActivity.java
+++ FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/MainActivity.java
@@ -6,7 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
--- FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/SendActivity.java
+++ FirebaseMessageDemo/app/src/main/java/edu/cs4730/firebasemessagedemo/SendActivity.java
@@ -1,6 +1,6 @@
 package edu.cs4730.firebasemessagedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ProgressDialog;
 import android.view.View;
@@ -13,11 +13,9 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
