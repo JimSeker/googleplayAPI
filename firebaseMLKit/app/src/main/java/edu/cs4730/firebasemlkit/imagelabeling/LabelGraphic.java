85988bb5bb299e4a7501de0ff7ae74e501ea9b06
Jim Ward <seker@uwyo.edu>
2019-02-19 13:49:03 -0700

    updated and fixed broken API calls.

--- firebaseMLKit/app/src/main/java/edu/cs4730/firebasemlkit/imagelabeling/LabelGraphic.java
+++ firebaseMLKit/app/src/main/java/edu/cs4730/firebasemlkit/imagelabeling/LabelGraphic.java
@@ -17,37 +17,40 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+
 import edu.cs4730.firebasemlkit.common.GraphicOverlay;
 import edu.cs4730.firebasemlkit.common.GraphicOverlay.Graphic;
 
 import java.util.List;
 
-/** Graphic instance for rendering a label within an associated graphic overlay view. */
+/**
+ * Graphic instance for rendering a label within an associated graphic overlay view.
+ */
 public class LabelGraphic extends Graphic {
 
-  private final Paint textPaint;
-  private final GraphicOverlay overlay;
+    private final Paint textPaint;
+    private final GraphicOverlay overlay;
 
-  private final List<FirebaseVisionLabel> labels;
+    private final List<FirebaseVisionImageLabel> labels;
 
-  LabelGraphic(GraphicOverlay overlay, List<FirebaseVisionLabel> labels) {
-    super(overlay);
-    this.overlay = overlay;
-    this.labels = labels;
-    textPaint = new Paint();
-    textPaint.setColor(Color.WHITE);
-    textPaint.setTextSize(60.0f);
-  }
-
-  @Override
-  public synchronized void draw(Canvas canvas) {
-    float x = overlay.getWidth() / 4.0f;
-    float y = overlay.getHeight() / 2.0f;
-
-    for (FirebaseVisionLabel label : labels) {
-      canvas.drawText(label.getLabel(), x, y, textPaint);
-      y = y - 62.0f;
+    LabelGraphic(GraphicOverlay overlay, List<FirebaseVisionImageLabel> labels) {
+        super(overlay);
+        this.overlay = overlay;
+        this.labels = labels;
+        textPaint = new Paint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(60.0f);
     }
-  }
+
+    @Override
+    public synchronized void draw(Canvas canvas) {
+        float x = overlay.getWidth() / 4.0f;
+        float y = overlay.getHeight() / 2.0f;
+
+        for (FirebaseVisionImageLabel label : labels) {
+            canvas.drawText(label.getText(), x, y, textPaint);
+            y = y - 62.0f;
+        }
+    }
 }
