87191c1180b61949c9581da5fc8d1a0bb03ff968
Jim Ward <seker@uwyo.edu>
2020-11-09 11:47:40 -0700

    updated the maps to bottomnavview.

--- mapDemov2/app/src/main/java/edu/cs4730/mapdemov2/MainActivity.java
+++ mapDemov2/app/src/main/java/edu/cs4730/mapdemov2/MainActivity.java
@@ -1,40 +1,26 @@
 package edu.cs4730.mapdemov2;
 
-
-import android.content.res.Configuration;
-import com.google.android.material.navigation.NavigationView;
-import androidx.fragment.app.FragmentManager;
-import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-//note, very little happens in here.  see the other activities.
-
-//Second note, this example needs to be updated to use fragments (a view pager?) instead of 3 activities.
+/**
+ * Very little is happening here, except seating up the bottom nav view for the three
+ * fragments.   check the basic for simple version, compass that shows which direction hybrid map, and drawmap for how to draw on a map.
+ */
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private DrawerLayout mDrawerlayout;
-    private NavigationView mNavigationView;
-    FragmentManager fragmentManager;
 
     static final LatLng CHEYENNE = new LatLng(41.1400, -104.8197);  //Note, West is a negative, East is positive
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     static final LatLng LARAMIE = new LatLng(41.312928, -105.587253);
-
-   BasicMapFragment BasicMapFrag;
-   CompassFragment CompassFrag;
-    DrawMapFragment DrawMapFrag;
-
 
     String TAG = "MainActivity";
 
@@ -42,116 +28,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //use the v7.toolbar instead of the default one.
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        //standard navigation drawer setup.
-        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        mDrawerToggle = new ActionBarDrawerToggle(this,  // host activity
-                mDrawerlayout,  //drawerlayout object
-                toolbar,  //toolbar
-                R.string.drawer_open,  //open drawer description  required!
-                R.string.drawer_close) {  //closed drawer description
-
-            //called once the drawer has closed.
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle("Categories");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            //called when the drawer is now open.
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                getSupportActionBar().setTitle(R.string.app_name);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        BasicMapFrag = new BasicMapFragment();
-        CompassFrag = new CompassFragment();
-        DrawMapFrag = new DrawMapFragment();
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
 
 
-
-        //To disable the icon for the drawer, change this to false
-        //mDrawerToggle.setDrawerIndicatorEnabled(true);
-        mDrawerlayout.addDrawerListener(mDrawerToggle);
-
-        //this ia the support Navigation view.
-        mNavigationView = (NavigationView) findViewById(R.id.navview);
-        //setup a listener, which acts very similar to how menus are handled.
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                //we could just as easily call onOptionsItemSelected(menuItem) and how it deal with it.
-                //Log.v(TAG, "We got someting?");
-                int id = menuItem.getItemId();
-                if (id == R.id.navigation_item_1) {
-                    //load fragment
-                    if (!menuItem.isChecked()) {  //only need to do this if fragment is already loaded.
-                        menuItem.setChecked(true);  //make sure to check/highlight the item.
-                        //fragmentManager.beginTransaction().replace(R.id.container, new BasicMapFragment()).commit();
-                        fragmentManager.beginTransaction().replace(R.id.container, BasicMapFrag).commit();
-                    }
-                    mDrawerlayout.closeDrawers();  //close the drawer, since the user has selected it.
-                    return true;
-                } else if (id == R.id.navigation_item_2) {
-                    //Log.v(TAG, "item 2?");
-                    //load fragment
-                    if (!menuItem.isChecked()) {  //only need to do this if fragment is already loaded.
-                        menuItem.setChecked(true); //make sure the item is checked/highlighted
-                        Log.v(TAG, "fab fragment?");
-                        //fragmentManager.beginTransaction().replace(R.id.container, new CompassFragment()).commit();
-                        fragmentManager.beginTransaction().replace(R.id.container, CompassFrag).commit();
-                    }
-                    //now close the nav drawer.
-                    mDrawerlayout.closeDrawers();
-                    return true;
-                } else if (id == R.id.navigation_item_3) {
-                    //load fragment
-                    //if (!menuItem.isChecked()) {  //only need to do this if fragment is already loaded.
-                    menuItem.setChecked(true);
-                    //fragmentManager.beginTransaction().replace(R.id.container, new DrawMapFragment()).commit();
-                    fragmentManager.beginTransaction().replace(R.id.container, DrawMapFrag).commit();
-                    //}
-                    mDrawerlayout.closeDrawers();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        //finally deal with the fragments.
-        fragmentManager = getSupportFragmentManager();
-        //first instance, so the default is zero.
-        fragmentManager.beginTransaction().replace(R.id.container, new BasicMapFragment()).commit();
     }
 
 
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
 }
