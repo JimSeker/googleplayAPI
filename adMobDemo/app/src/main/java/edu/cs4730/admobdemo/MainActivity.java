3e22c6714681c00cceb72c949f2c0e1d49f442a3
Jim Ward <seker@uwyo.edu>
2021-01-25 11:38:09 -0700

    updated ad code to new API

--- adMobDemo/app/src/main/java/edu/cs4730/admobdemo/MainActivity.java
+++ adMobDemo/app/src/main/java/edu/cs4730/admobdemo/MainActivity.java
@@ -2,6 +2,7 @@ package edu.cs4730.admobdemo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,15 +13,30 @@ import com.google.ads.consent.ConsentInfoUpdateListener;
 import com.google.ads.consent.ConsentInformation;
 import com.google.ads.consent.ConsentStatus;
 import com.google.ads.consent.DebugGeography;
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
+
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+/**
+ * api 19.7.0 is a trans version to API 20 for the ads and many things are depreciated.
+ *  I'm attempting to use the newer methods.
+ */
+
 
 public class MainActivity extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
     private static String TAG = "MainActivity";
+    AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +46,46 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
 
+        //MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");  //19.5.0 version.
+        MobileAds.initialize(this, new OnInitializationCompleteListener() { //19.7.0+ version.
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Log.wtf(TAG, "Initialization is completed.");
+            }
+        });
 
         //for the ad at the bottom of the mainactivity.
-        AdView mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice("D1A4B2E34EF63965FDB3E19C432D0D82").build();
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
 
-        //for the Interstitial ad, which is launched via the button
-        mInterstitialAd = new InterstitialAd(this);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.d(TAG, "banner ad has finished loading.");
+            }
 
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.d(TAG, "banner ad has failed to load.");
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+                Log.d(TAG, "banner ad is displayed.");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                Log.d(TAG, "banner ad has closed, now do something else.");
+            }
+        });
+
+        mAdView.loadAd(adRequest);
 
         ConsentInformation consentInformation = ConsentInformation.getInstance(this);
         consentInformation.addTestDevice("D1A4B2E34EF63965FDB3E19C432D0D82");
@@ -61,54 +106,65 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        mAdView.loadAd(adRequest);
 
 
-        mInterstitialAd.loadAd(adRequest);
+        //for the Interstitial ad, which is launched via the button
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // The mInterstitialAd reference will be null until
+                // an ad is loaded.
+                mInterstitialAd = interstitialAd;
+
+                //now we can setup the full screen pieces.
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // Called when fullscreen content is dismissed.
+                        Log.d("TAG", "The ad was dismissed.");
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // Called when fullscreen content failed to show.
+                        Log.d("TAG", "The ad failed to show.");
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        // Make sure to set your reference to null so you don't
+                        // show it a second time.
+                        mInterstitialAd = null;
+                        Log.d("TAG", "The ad was shown.");
+                    }
+                });
+
+
+                Log.i(TAG, "onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                // Handle the error
+                Log.i(TAG, loadAdError.getMessage());
+                mInterstitialAd = null;
+            }
+        });
+
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(MainActivity.this);
                 } else {
                     Log.d(TAG, "The interstitial wasn't loaded yet.");
                 }
 
             }
         });
-        //listener for teh interstitial ad, so we can react as needed.
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-                Log.d(TAG, "ad has finished loading.");
-            }
 
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-                Log.d(TAG, "ad has failed to load.");
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when the ad is displayed.
-                Log.d(TAG, "ad is displayed.");
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-                Log.d(TAG, "User has left the app (via the add).");
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when when the interstitial ad is closed.
-                Log.d(TAG, "ad has closed, now do something else.");
-            }
-        });
     }
 
 }
