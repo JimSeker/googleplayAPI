83baae2cb04955ae688b6a1023efd37fd51c2fbb
Jim Ward <JimSeker@users.noreply.github.com>
2021-01-26 11:25:25 -0700

    example now works in API 30.

--- AndroidBeaconLibrary/app/src/main/java/edu/cs4730/androidbeaconlibrary/MainActivity.java
+++ AndroidBeaconLibrary/app/src/main/java/edu/cs4730/androidbeaconlibrary/MainActivity.java
@@ -1,15 +1,25 @@
 package edu.cs4730.androidbeaconlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * very beginning of a beacon replacement example since google is dropping all support.
@@ -24,13 +34,25 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
     private BeaconManager beaconManager;
     private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUESTS = 1;
+    TextView logger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        logger = findViewById(R.id.logger);
+        logger.setText("App started\n");
+        //check for permissions and start the beacons.
         beaconManager = BeaconManager.getInstanceForApplication(this);
+        //added eddystone, since I'm moving from google's beacons to altbeacon.  RedBeacon can broaccast both.
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT) );
+        if (allPermissionsGranted()) {
+            logger.append("Starting beaconManager\n");
+            beaconManager.bind(this);
+        } else {
+            getRuntimePermissions();
+        }
     }
 
     @Override
@@ -40,22 +62,91 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             @Override
             public void didEnterRegion(Region region) {
                 Log.wtf(TAG, "I just saw an beacon for the first time!");
+                logger.append("I just saw an beacon for the first time!\n");
             }
 
             @Override
             public void didExitRegion(Region region) {
                 Log.wtf(TAG, "I no longer see an beacon");
+                logger.append("I no longer see an beacon\n");
             }
 
             @Override
             public void didDetermineStateForRegion(int state, Region region) {
                 Log.wtf(TAG, "I have just switched from seeing/not seeing beacons: " + state);
+                logger.append("I have just switched from seeing/not seeing beacons: " + state + "\n");
             }
         });
 
         try {
             beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+            logger.append("Added the listener piece. \n");
         } catch (RemoteException e) {
+            logger.append("failed to add the listener piece. \n");
+        }
+
+    }
+
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
         }
     }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        Log.i(TAG, "Permission granted!");
+        if (allPermissionsGranted()) {
+            logger.append("Starting beaconManager");
+            beaconManager.bind(this);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission granted: " + permission);
+            return true;
+        }
+        Log.i(TAG, "Permission NOT granted: " + permission);
+        return false;
+    }
+
 }
