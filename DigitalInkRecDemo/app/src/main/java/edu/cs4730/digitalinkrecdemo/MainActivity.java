9501501e2ae4f7212cc162f761c3c01930f802f2
Jim Ward <seker@uwyo.edu>
2021-02-03 19:55:12 -0700

    attempt at new example.

---
+++ DigitalInkRecDemo/app/src/main/java/edu/cs4730/digitalinkrecdemo/MainActivity.java
@@ -0,0 +1,136 @@
+package edu.cs4730.digitalinkrecdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.common.MlKitException;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognition;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModel;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModelIdentifier;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizer;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizerOptions;
+import com.google.mlkit.vision.digitalink.Ink;
+import com.google.mlkit.vision.digitalink.RecognitionResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int boardsize = 480;
+    Bitmap theboard;
+    Canvas theboardc;
+    float startx, starty;
+    Button button;
+    ImageView iv;
+    TextView logger;
+    Ink.Builder inkBuilder;
+    Ink.Stroke.Builder strokeBuilder;
+    Paint paint;
+    DigitalInkRecognizer recognizer;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        iv = findViewById(R.id.imageView);
+        logger = findViewById(R.id.logger);
+
+
+        theboard = Bitmap.createBitmap(boardsize, boardsize, Bitmap.Config.ARGB_8888);
+        theboardc = new Canvas(theboard);
+        theboardc.drawColor(Color.WHITE);  //background color for the board.
+        iv.setImageBitmap(theboard);
+
+        inkBuilder = Ink.builder();
+        paint = new Paint();
+
+        iv.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                float x = event.getX();
+                float y = event.getY();
+                long t = System.currentTimeMillis();
+
+                // If your setup does not provide timing information, you can omit the
+                // third parameter (t) in the calls to Ink.Point.create
+                int action = event.getActionMasked();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        strokeBuilder = Ink.Stroke.builder();
+                        strokeBuilder.addPoint(Ink.Point.create(x, y, t));
+                        startx = x;
+                        starty = y;
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        strokeBuilder.addPoint(Ink.Point.create(x, y, t));
+                        theboardc.drawLine(startx, starty, x, y, paint);
+                        iv.setImageBitmap(theboard);
+                        startx = x;
+                        starty = y;
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        strokeBuilder.addPoint(Ink.Point.create(x, y, t));
+                        inkBuilder.addStroke(strokeBuilder.build());
+                        strokeBuilder = null;
+                        theboardc.drawLine(startx, starty, x, y, paint);
+                        iv.setImageBitmap(theboard);
+                        return true;
+                }
+                return false;
+            }
+        });
+        DigitalInkRecognitionModelIdentifier modelIdentifier = null;
+        try {
+            modelIdentifier = DigitalInkRecognitionModelIdentifier.fromLanguageTag("en-US");
+        } catch (MlKitException e) {
+            // language tag failed to parse, handle error.
+        }
+        if (modelIdentifier != null) {
+            DigitalInkRecognitionModel model = DigitalInkRecognitionModel.builder(modelIdentifier).build();
+            // Get a recognizer for the language
+            recognizer = DigitalInkRecognition.getClient(DigitalInkRecognizerOptions.builder(model).build());
+        } else {
+            recognizer = null;
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ink ink = inkBuilder.build();
+                if (recognizer != null) {
+                    recognizer.recognize(ink)
+                        .addOnSuccessListener(new OnSuccessListener<RecognitionResult>() {
+                            @Override
+                            public void onSuccess(RecognitionResult result) {
+                                logger.append("result is " + result.getCandidates().get(0).getText());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                logger.append("Failed to recognize.");
+                            }
+                        });
+                }
+            }
+        });
+    }
+}
