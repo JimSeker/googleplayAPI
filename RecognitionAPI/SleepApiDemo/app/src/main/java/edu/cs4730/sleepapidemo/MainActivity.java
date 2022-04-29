ae095b34b6639fda5b166debbb02efdeb9272fea
Jim Ward <seker@uwyo.edu>
2022-04-29 10:16:56 -0600

    updated to API 32

--- RecognitionAPI/SleepApiDemo/app/src/main/java/edu/cs4730/sleepapidemo/MainActivity.java
+++ RecognitionAPI/SleepApiDemo/app/src/main/java/edu/cs4730/sleepapidemo/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint({"MissingPermission", "UnspecifiedImmutableFlag"})
         //it's already handled.
     void subscribeToSleep() {
         PendingIntent pi;
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint({"MissingPermission", "UnspecifiedImmutableFlag"})
     void unsubscriptToSleep() {
         PendingIntent pi;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
